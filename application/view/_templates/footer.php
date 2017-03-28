ea1347f18ff65ee4e65cbcb473483b011110d33d
despina <despinut@yahoo.com>
2017-03-28 13:47:10 +0300

    First commit

---
+++ application/view/_templates/footer.php
@@ -0,0 +1,32 @@
+</div><!--/row-->
+</div><!--container-->
+    <!-- backlink to repo on GitHub, and affiliate link to Rackspace if you want to support the project -->
+    <div class="footer">
+        Find <a href="https://github.com/panique/mini">MINI on GitHub</a>.
+        If you like the project, support it by <a href="http://tracking.rackspace.com/SH1ES">using Rackspace</a> as your hoster [affiliate link].
+    </div>
+
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css?34567">
+
+    <!-- Simplex theme -->
+    <link rel="stylesheet" href="https://bootswatch.com/simplex/bootstrap.min.css?34568">
+        
+    <!-- Latest compiled and minified JavaScript -->
+    <!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js?5655"></script>
+    <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
+    <script>
+        var url = "<?php echo URL; ?>";
+    </script>
+
+    <!-- our JavaScript -->
+    <script src="<?php echo URL; ?>js/application.js"></script>
+</body>
+</html>
---
+++ application/view/_templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>MINI</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- JS -->
+        <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+        <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+
+        <!-- CSS -->
+        <link href="<?php echo URL; ?>css/style.css?1234" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <!-- logo -->
+                <div class="logo">
+                    MINI
+                </div>
+
+                <!-- navigation -->
+                <div class="navigation">
+                    <a href="<?php echo URL; ?>">home</a>
+                    <a href="<?php echo URL; ?>home/exampleone">subpage</a>
+                    <a href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
+                    <a href="<?php echo URL; ?>songs">songs</a>
+                </div>
